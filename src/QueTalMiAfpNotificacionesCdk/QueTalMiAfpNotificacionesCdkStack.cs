8105a52528bf25f9d76605a35aae8910d40426ff
Benjamín <benjamin.diaze@outlook.com>
2025-09-07 16:23:22 -0300

    Initial commit

---
+++ src/QueTalMiAfpNotificacionesCdk/QueTalMiAfpNotificacionesCdkStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace QueTalMiAfpNotificacionesCdk
+{
+    public class QueTalMiAfpNotificacionesCdkStack : Stack
+    {
+        internal QueTalMiAfpNotificacionesCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
