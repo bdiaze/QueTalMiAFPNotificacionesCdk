d5d8851a28a38ee942ba4110f9a32333f6371b68
Benjamín <benjamin.diaze@outlook.com>
2025-09-09 23:44:38 -0300

    Se añade modelo del dao de cuotaufcomision

---
+++ LambdaNotificacion/LambdaNotificacion/Models/EntObtenerUltimaCuota.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaNotificacion.Models {
+    internal class EntObtenerUltimaCuota {
+        public required string ListaAFPs { get; set; }
+        public required string ListaFondos { get; set; }
+        public required string ListaFechas { get; set; }
+        public required int TipoComision { get; set; }
+    }
+}
---
+++ LambdaNotificacion/LambdaNotificacion/Models/SalObtenerUltimaCuota.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaNotificacion.Models {
+    internal class SalObtenerUltimaCuota {
+        public required string Afp { get; set; }
+        public required DateTime Fecha { get; set; }
+        public required string Fondo { get; set; }
+        public required decimal Valor { get; set; }
+        public decimal? Comision { get; set; }
+    }
+}
